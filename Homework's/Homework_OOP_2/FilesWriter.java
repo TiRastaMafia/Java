59d394522eb6d53ef0f8e80c0fd29c7fb90a3d15
TiRasTaMafia <vladislav.stanchu@yandex.ru>
2022-12-17 22:11:33 +0300

    Add files via upload

---
+++ Homework's/Homework_OOP_2/FilesWriter.java
@@ -0,0 +1,5 @@
+import java.util.Map;
+
+public abstract class FilesWriter {
+    public abstract void writer(Map<String, Double> args);
+}
---
+++ Homework's/Homework_OOP_2/TxtWriter.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+
+// import javax.sql.rowset.spi.XmlWriter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TxtWriter extends FilesWriter {
+
+    @Override
+    public void writer(Map<String, Double> args) {
+
+        try (PrintWriter file = new PrintWriter("file1.txt", "UTF-8")){
+
+            for (Map.Entry<String, Double> entry : args.entrySet()) {
+                    file.println(entry);
+                    }
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}

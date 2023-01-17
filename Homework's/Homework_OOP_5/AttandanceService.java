6852595ad0eac7558023d994605f42e84876312b
TiRasTaMafia <vladislav.stanchu@yandex.ru>
2023-01-17 14:12:47 +0300

    last commit

---
+++ Homework's/Homework_OOP_5/AttandanceService.java
@@ -0,0 +1,29 @@
+// package Homework_OOP_3;
+
+import java.util.List;
+
+// import Homework_OOP_3.date.*;
+
+public class AttandanceService { // База данных, хранилище всех данных студентов "Model", должна быть с
+                                 // обобщением, чтобы можно было использовать не только с группой студентов
+    private StudentsGroup sGroup = new StudentsGroup();
+
+    public AttandanceService() {
+        sGroup.addStudentInGroup(new Student("Ivanov", "Vasiliy"));
+        sGroup.addStudentInGroup(new Student("Petrov", "Evklid"));
+        sGroup.addStudentInGroup(new Student("Amarov", "Karen"));
+        sGroup.addStudentInGroup(new Student("Kluny", "Djorge"));
+        sGroup.addStudentInGroup(new Student("Sean", "Poll"));
+    }
+
+    @Override
+    public String toString() {
+        return sGroup.toString();
+    }
+
+    public List<Student> getsGroup() { // получение списка группы студентов
+        List<Student> copyStudentsGroup = sGroup.getStList();
+        return copyStudentsGroup;
+    }
+
+}
---
+++ Homework's/Homework_OOP_5/AttandanceView.java
@@ -0,0 +1,14 @@
+
+import java.util.List;
+
+// import Homework_OOP_3.date.Student;
+
+public class AttandanceView { // View
+
+    public void printResult(List<Student> lst) {
+        for (Object elem : lst) {
+            System.out.println(elem);
+        }
+    }
+
+}
